cef074bed20920939d90277c54fa74c7df185336
Alex Shapiro <acjshapiro@gmail.com>
2018-02-19 14:57:51 -0700

    RPS

--- rubygolf.rb
+++ rubygolf.rb
@@ -1,6 +1,24 @@
+#hole 1
+#
+# def sum_array(a)
+#   a.inject(:+)
+# end
+# a = [1,2,3]
+# puts sum_array(a)
 
-def sum_array(a)
-  a.inject(:+)
+#hole 2
+
+def rps
+  c = ['r', 'p', 's'].sample
+  puts "pick: rock(r) / paper(p) / scissor(s)"
+  u = gets.strip
+  puts "you chose #{u} computer chose #{c}"
+  if u == 'r' && c == 'p' || u == 'p' && c == 's' || u == 's' && c == 'r'
+    puts "you lose"
+  elsif u == 'r' && c == 's' || u == 'p' && c == 'r' || u == 's' && c == 'p'
+    puts "you win"
+  else
+    tie
+  end
 end
-a = [1,2,3]
-puts sum_array(a)
+rps
