b44da7365ffb65142681887dd9b7ad7d164598e5
Alex Shapiro <acjshapiro@gmail.com>
2018-02-19 15:36:50 -0700

    hole 3

--- rubygolf.rb
+++ rubygolf.rb
@@ -6,19 +6,45 @@
 # a = [1,2,3]
 # puts sum_array(a)
 
-#hole 2
+# hole 2
+# def rps_cheat
+#      r =['Win','Lose','Tie'].sample
+#      c =['r', 'r', 's'].sample
+#     puts "choose r, p or s"
+#     gets
+#   puts r
+# end
+#
+# def rps
+#   c = ['r', 'p', 's'].sample
+#   puts "pick: rock(r) / paper(p) / scissor(s)"
+#   u = gets
+#   puts "you chose #{u} computer chose #{c}"
+#   if u == c
+#     puts "tie"
+#   else u == 'r' && c == 'p' || u == 'p' && c == 's' || u == 's' && c == 'r'
+#     puts "you lose"
+#   else
+#     puts "win"
+#   end
+# end
+# # rps
 
-def rps
-  c = ['r', 'p', 's'].sample
-  puts "pick: rock(r) / paper(p) / scissor(s)"
-  u = gets.strip
-  puts "you chose #{u} computer chose #{c}"
-  if u == 'r' && c == 'p' || u == 'p' && c == 's' || u == 's' && c == 'r'
-    puts "you lose"
-  elsif u == 'r' && c == 's' || u == 'p' && c == 'r' || u == 's' && c == 'p'
-    puts "you win"
-  else
-    tie
+#hole 3
+
+def fizzbuzz
+  a = [*1..100]
+  a.each do |i|
+    if i % 15 == 0
+      puts "fizzbuzz"
+    elsif i % 3 == 0
+      puts "fizz"
+    elsif i % 5 == 0
+      puts "buzz"
+    else
+      puts i
+    end
   end
 end
-rps
+
+fizzbuzz
